c6cd34360aaff47a9b611198dd3a8078e52142d1
Trevor Stubbs <stubbste@gmail.com>
2020-07-21 14:00:30 -0700

    db setup

---
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+
+    }
+}
---
+++ AsyncInn/AsyncInn/Models/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Hotel
+    {
+        public int Id { set; get; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+    }
+}
