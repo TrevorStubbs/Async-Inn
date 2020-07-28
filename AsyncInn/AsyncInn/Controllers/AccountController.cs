53d297ca8daad99a7c71019f2dd4e5c370eb7e0a
Trevor Stubbs <stubbste@gmail.com>
2020-07-28 11:50:26 -0700

    authentication complete

--- AsyncInn/AsyncInn/Controllers/AccountController.cs
+++ AsyncInn/AsyncInn/Controllers/AccountController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AsyncInn.Models;
+using AsyncInn.Models.DTOs;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AsyncInn.Controllers
@@ -11,5 +14,41 @@ namespace AsyncInn.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // api/account/register
+        [HttpPost,Route("register")]
+        public async Task<IActionResult> Register(RegisterDTO register)
+        {
+            // do something to put this into the database
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = register.Email,
+                UserName = register.Email,
+                FirstName = register.FirstName,
+                LastName = register.LastName
+            };
+
+            //create the user
+            var result = await _userManager.CreateAsync(user, register.Password);
+
+            if (result.Succeeded)
+            {
+                // sign the user in if it was successful.
+                await _signInManager.SignInAsync(user, false);
+
+                return Ok();
+
+            }
+            return BadRequest("Invalid Registration");
+
+        }
     }
 }
